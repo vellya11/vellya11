774c0f0f89ef853a5b5376e8acfd8855413ca17e
vellya11 <93827443+vellya11@users.noreply.github.com>
2022-05-26 23:05:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PROJECT 6 - NOUVELLYA PFA.</title>
+</head>
+
+<body>
+
+</body>
+<h3>NOUVELLYA PUTRI FARRADILLAH AYUNIKENTI - DIGITALENT</h3>
+<a href="10th/barang_data.php">Project 3</a>
+<br>
+<a href="12nd/index.php">Project 4</a>
+<br>
+<a href="13rd/barang_data.php">Project 5</a>
+</body>
+
+</html>
